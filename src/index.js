fb642aaef4c20c286577c26aa93cf35786f501a7
anironL <viasolaris@gmail.com>
2023-01-09 13:12:08 -0500

    Package Update
    update react scripts to v5.0.1

--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Return an HTML <button> from the Button functional component. 
+// Use props.children in a way that will allow you to pass text easily 
+// (i.e. by typing the text between the <button> tags).
+
+// Put a <Button> inside the Application component between the <main> tags above the <h1>. 
+// Pass the <Button> the text "Reset".
 const Button = (props) => {
-  // your code here
+  const buttonText = props.buttonText;
+  const clickEvent = props.onClick;
+
+  return(
+    <button onClick={clickEvent}>
+      {buttonText}
+    </button>
+  )  
 };
 
 const Application = () => {
-
   // your code here
+  const [name, setName] = useState("Yike")
 
   const reset = () => {
     console.log("reset");
@@ -17,8 +30,9 @@ const Application = () => {
 
   return (
     <main>
-      {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <input placeholder="Type your name"></input>
+      <Button onClick={reset} buttonText="Reset" />
+      <h1>Hello {name}</h1>
     </main>
   );
 };
